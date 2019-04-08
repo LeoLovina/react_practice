e65e142774abe8607f2bef1598b30253b71c8f1c
leo <leo.lovina@gmail.com>
2019-04-08 16:58:29 +0800

    Add Nested Routes

--- react-webpack/src/routerTest/Notfound.tsx
+++ react-webpack/src/routerTest/Notfound.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react'
 
-export default class Notfound extends React.Component {
+// export default class Notfound extends React.Component {
 
-    render(){
-        return (
-            <div> <h1> Not Found</h1></div>
-        );
-    }
-}
+//     render(){
+//         return (
+//             <div> <h1> Not Found</h1></div>
+//         );
+//     }
+// }
+
+const Notfound = () => <h1>Not Found</h1>
+export default Notfound;
--- react-webpack/src/routerTest/Users.tsx
+++ react-webpack/src/routerTest/Users.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+const User = ({ match } : any) => <p> {match.parms.id}</p>;
 
 export default class Users extends React.Component<any, any> {
     constructor(props: any) {
@@ -10,12 +13,26 @@ export default class Users extends React.Component<any, any> {
 
     render() {
         console.log(this.props);
-        const {params} =this.props.match;
+        const { params } = this.props.match;
         return (
             <div>
                 <h1>User</h1>
+                <strong>select a user</strong>
                 <p> {params.id} </p>
+                <ul>
+                    <li>
+                        <Link to="/users/1">User 1 </Link>
+                    </li>
+                    <li>
+                        <Link to="/users/2">User 2 </Link>
+                    </li>
+                    <li>
+                        <Link to="/users/3">User 3 </Link>
+                    </li>
+                </ul>
+                <Route path="/users/:id" component={User} />
             </div>
         );
     };
 }
+
