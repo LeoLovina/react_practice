0445ba931e6d77965d8019e7e04e36d83b69aed8
leo_lovina <leo.lovina@gmail.com>
2019-10-22 00:06:40 +0800

    review router

---
+++ my-app/src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/user">User</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>            
+        </div>
+    );
+}
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,19 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import HelloMessage from './myApp';
-
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import Home from './components/Home';
+import User from './components/User';
+import Contact from './components/Contact';
+import UserDetail from './components/UserDetail';
+
+const routing = (
+  <Router>
+    <div>
+      <Route exact={true} path="/" component = {Home} />
+      <Route path="/User" component= {User}/>
+      <Route path="/Contact" component= {Contact}/>
+      <Route path="/user/:id" component={UserDetail} />
+    </div>
+  </Router>
 );
 
 ReactDOM.render(
-  <HelloMessage />,
-  document.getElementById('myApp') as HTMLElement
+  routing,
+  document.getElementById('root') as HTMLElement
 );
 
 registerServiceWorker();
