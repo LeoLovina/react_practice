a6ef9dd86f2e385a6d4f40c6adae78b4f3682b61
Leo Chen <leo.lovina@gmail.com>
2020-07-28 22:12:38 +0800

    add style

--- my-app-js/src/components/LoginBox.js
+++ my-app-js/src/components/LoginBox.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(2),
+        },
+    },
+}));
+
 export default function LoginBox() {
+    const classes = useStyles();
+
     const clickHandler = event => {
         alert('haha');
     }
     return (
-        <div>
-            <h1> LoginBox</h1>
-            <Button onClick = {clickHandler} > Button </Button>
+        <div className= {classes.root}>
+            <Button onClick={clickHandler} variant="contained" color="primary" > Button </Button>
+            <Button onClick={clickHandler} variant="contained" color="secondary" > Button </Button>
         </div>
     );
 };
