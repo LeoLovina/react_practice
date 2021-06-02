3dab7bc184d4858e6c77b86e0b588b842b68a0d8
Leo Chen <lchen@equuscs.com>
2021-06-02 09:43:30 +0800

    update basic example

--- react_bootstrap/src/App.tsx
+++ react_bootstrap/src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home';
+import User from './components/User';
+import Contact from './components/Contact';
+import UserDetail from './components/UserDetail';
+class App extends React.Component {
+  public render() {
+    return (
+      <Router>
+      <div>
+        <Route exact={true} path="/" component = {Home} />
+        <Route path="/User" component= {User}/>
+        <Route path="/Contact" component= {Contact}/>
+        <Route path="/user/:id" component={UserDetail} />
+      </div>
+    </Router>
+    );
+  }
 }
 
 export default App;
