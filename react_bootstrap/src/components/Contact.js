73b32f627230a7906e8dfde115de1dee17446ef9
Leo Chen <lchen@equuscs.com>
2021-06-02 16:08:58 +0800

    npm install bootstrap,  react-bootstrap, node-sass. sass

--- react_bootstrap/src/components/Contact.js
+++ react_bootstrap/src/components/Contact.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
 
-export default function Contact() {
+export default function Contact({id, name}) {
     return (
         <div>
-            <h1>Contact</h1>            
+            <h1>Contact</h1>
+            <Button>This is default button</Button>
+            <Button variant="secondary" size="sm" disabled>Secondary</Button>
+            <Button variant="success">Success</Button>
+            <Button variant="info">Info</Button>
         </div>
     );
 }
