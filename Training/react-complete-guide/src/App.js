6a6b994b30c7a03bcb0d3f31aabeb32b22ebca54
Leo Chen <lchen@equuscs.com>
2022-06-20 21:24:59 +0800

    change to arrow function

--- Training/react-complete-guide/src/App.js
+++ Training/react-complete-guide/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Expenses from './components/Expenses/Expenses';
 
 
-function App() {
+const App = () => {
   const expenses = [
     { id:'1', title:'Toilet Paper', amount: 94.12, date: new Date(2022,1,20)},
     { id:'2', title:'Car Insurance', amount: 194.12, date: new Date(2022,5,2)},
--- Training/react-complete-guide/src/components/Expenses/Expenses.js
+++ Training/react-complete-guide/src/components/Expenses/Expenses.js
@@ -2,12 +2,8 @@ import './Expenses.css';
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 
-function Expenses(props) {
-    // const expenses = [
-    //     { id:'1', title:'Toilet Paper', amount: 94.12, date: new Date(2022,1,20)},
-    //     { id:'2', title:'Car Insurance', amount: 194.12, date: new Date(2022,5,2)},
-    //     { id:'3', title:'Food', amount: 94.12, date: new Date(2022,1,12)}
-    //   ]
+const Expenses = (props) => {
+
     return (
         <Card className='expenses'>
         <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
